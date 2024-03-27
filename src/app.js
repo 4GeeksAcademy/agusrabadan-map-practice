48455e0c2996fa2c3679eca3e07005212ca80839
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-03-27 19:45:32 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -16,32 +16,48 @@ window.onload = function () {
   let dom = [".com", ".es", ".org", ".ar"];
 
 
-//Metodo for
-  
-let textFor = '<ul class="list-group">'
-  textFor += '<li class="list-group-item> Using for length </li>';
+  //Metodo for
 
-  for (let i = 0; i < art.length; i++); {
-    for (let j = 0; j < adj.length; j++); {
-      for (let k = 0; k < noun.length; k++); {
-        for (let l = 0; l < dom.length; l++); {
-          textFor += '<li class="list-group-item">' + pronoun[i] + adj[j] + noun[k] + dom[l] + "</li>"
+  let textFor = `<ul class="list-group">`
+
+  textFor += `<li class="list-group-item bg-warning text-light">Loop For </li>`
+
+  for (let i = 0; i < art.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          textFor += `<li class="list-group-item">${art[i]}${adj[j]}${noun[k]}${dom[l]}</li>
+          `
         }
       }
     }
   }
 
 
-  textFor += "</ul>"
+  textFor += `</ul>`
 
   document.querySelector("#for").innerHTML = textFor
 
   //Map
-  
 
+  let textMap = `<ul class="list-group">`
 
+  textMap += `<li class="list-group-item bg-danger">Loop Map</li>`
 
+  art.map((item) => {
+    adj.map((element) => {
+      noun.map((iterator) => {
+        dom.map((dominio) => {
+          textMap += `
+          <li class="list-group-item">${item}${element}${iterator}${dominio}</li>
+          `
+        })
+      })
+    })
+  })
+
+  textMap += `</ul>`;
+
+  document.querySelector("#map").innerHTML = textMap;
 
 };
-
-

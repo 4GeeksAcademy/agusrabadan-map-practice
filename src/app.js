df5c6577163b89f6f01d90925ff770cdc1e58dd2
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-01 16:28:32 +0000

    inicio

--- src/app.js
+++ src/app.js
@@ -5,260 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  
-  
-  
-  //Array
+window.onload = function () {
 
-const people = [
-  {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "imageUrl": "https://picsum.photos/id/317/200/200",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  },
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv",
-    "imageUrl": "https://picsum.photos/id/343/200/200",
-    "address": {
-      "street": "Victor Plains",
-      "suite": "Suite 879",
-      "city": "Wisokyburgh",
-      "zipcode": "90566-7771",
-      "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-      }
-    },
-    "phone": "010-692-6593 x09125",
-    "website": "anastasia.net",
-    "company": {
-      "name": "Deckow-Crist",
-      "catchPhrase": "Proactive didactic contingency",
-      "bs": "synergize scalable supply-chains"
-    }
-  },
-  {
-    "id": 3,
-    "name": "Clementine Bauch",
-    "username": "Samantha",
-    "email": "Nathan@yesenia.net",
-    "imageUrl": "https://picsum.photos/id/312/200/200",
-    "address": {
-      "street": "Douglas Extension",
-      "suite": "Suite 847",
-      "city": "McKenziehaven",
-      "zipcode": "59590-4157",
-      "geo": {
-        "lat": "-68.6102",
-        "lng": "-47.0653"
-      }
-    },
-    "phone": "1-463-123-4447",
-    "website": "ramiro.info",
-    "company": {
-      "name": "Romaguera-Jacobson",
-      "catchPhrase": "Face to face bifurcated interface",
-      "bs": "e-enable strategic applications"
-    }
-  },
-  {
-    "id": 4,
-    "name": "Patricia Lebsack",
-    "username": "Karianne",
-    "email": "Julianne.OConner@kory.org",
-    "imageUrl": "https://picsum.photos/id/195/200/200",
-    "address": {
-      "street": "Hoeger Mall",
-      "suite": "Apt. 692",
-      "city": "South Elvis",
-      "zipcode": "53919-4257",
-      "geo": {
-        "lat": "29.4572",
-        "lng": "-164.2990"
-      }
-    },
-    "phone": "493-170-9623 x156",
-    "website": "kale.biz",
-    "company": {
-      "name": "Robel-Corkery",
-      "catchPhrase": "Multi-tiered zero tolerance productivity",
-      "bs": "transition cutting-edge web services"
-    }
-  },
-  {
-    "id": 5,
-    "name": "Chelsey Dietrich",
-    "username": "Kamren",
-    "email": "Lucio_Hettinger@annie.ca",
-    "imageUrl": "https://picsum.photos/id/215/200/200",
-    "address": {
-      "street": "Skiles Walks",
-      "suite": "Suite 351",
-      "city": "Roscoeview",
-      "zipcode": "33263",
-      "geo": {
-        "lat": "-31.8129",
-        "lng": "62.5342"
-      }
-    },
-    "phone": "(254)954-1289",
-    "website": "demarco.info",
-    "company": {
-      "name": "Keebler LLC",
-      "catchPhrase": "User-centric fault-tolerant solution",
-      "bs": "revolutionize end-to-end systems"
-    }
-  },
-  {
-    "id": 6,
-    "name": "Mrs. Dennis Schulist",
-    "username": "Leopoldo_Corkery",
-    "email": "Karley_Dach@jasper.info",
-    "imageUrl": "https://picsum.photos/id/132/200/200",
-    "address": {
-      "street": "Norberto Crossing",
-      "suite": "Apt. 950",
-      "city": "South Christy",
-      "zipcode": "23505-1337",
-      "geo": {
-        "lat": "-71.4197",
-        "lng": "71.7478"
-      }
-    },
-    "phone": "1-477-935-8478 x6430",
-    "website": "ola.org",
-    "company": {
-      "name": "Considine-Lockman",
-      "catchPhrase": "Synchronised bottom-line interface",
-      "bs": "e-enable innovative applications"
-    }
-  },
-  {
-    "id": 7,
-    "name": "Kurtis Weissnat",
-    "username": "Elwyn.Skiles",
-    "email": "Telly.Hoeger@billy.biz",
-    "imageUrl": "https://picsum.photos/id/297/200/200",
-    "address": {
-      "street": "Rex Trail",
-      "suite": "Suite 280",
-      "city": "Howemouth",
-      "zipcode": "58804-1099",
-      "geo": {
-        "lat": "24.8918",
-        "lng": "21.8984"
-      }
-    },
-    "phone": "210.067.6132",
-    "website": "elvis.io",
-    "company": {
-      "name": "Johns Group",
-      "catchPhrase": "Configurable multimedia task-force",
-      "bs": "generate enterprise e-tailers"
-    }
-  },
-  {
-    "id": 8,
-    "name": "Nicholas Runolfsdottir V",
-    "username": "Maxime_Nienow",
-    "email": "Sherwood@rosamond.me",
-    "imageUrl": "https://picsum.photos/id/137/200/200",
-    "address": {
-      "street": "Ellsworth Summit",
-      "suite": "Suite 729",
-      "city": "Aliyaview",
-      "zipcode": "45169",
-      "geo": {
-        "lat": "-14.3990",
-        "lng": "-120.7677"
-      }
-    },
-    "phone": "586.493.6943 x140",
-    "website": "jacynthe.com",
-    "company": {
-      "name": "Abernathy Group",
-      "catchPhrase": "Implemented secondary concept",
-      "bs": "e-enable extensible e-tailers"
-    }
-  },
-  
-]
+  let palos = ["♠", "♣", "♥", "♦"];
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-//Lista
+  for(let i=0; i<3; i++){
+    for(let j=0; j<12; j++){
+      
+    } 
+  };
+      
 
-let textHTML= '<ul class="list-group">'
-
- people.map((item)=> 
-
- textHTML += `
- <li  class="list-group-item d-flex justify-content-between">
-   <div>${item.name}</div>
-   <div>
-     <span class="mx-1"><i class="far fa-eye"></i></span>
-     <span class="mx-1"><i class="far fa-edit"></i></span>
-     <span class="mx-1"><i class="fas fa-trash-alt text-danger"></i></span>
-   </div>
- </li>
-`
-)
-textHTML += '</ul>'
-
-//Cards
-let cardHTML = " "
-
-people.map((element)=>{
-  
-  cardHTML += `
-  
-  <div class="card mx-auto mb-5" style="width: 18rem;">
-  <img src= ${element.imageUrl} class="card-img-top" alt="...">
+  let text = `<div class="card mt-5 mx-auto" style="width: 18rem;">
+  <h3 class="card-title mt-3">${palos[i]}${numeros[j]}</h3>
   <div class="card-body">
-    <h5 class="card-title">${element.name}</h5>
-    <p class="card-text"> Company: ${element.company.name}</p>
+  <img src="https://image.spreadshirtmedia.net/image-server/v1/compositions/T6A1PA5835PT17X47Y34D302514130W21795H21795Cx000000:xFF0638/views/1,width=550,height=550,appearanceId=1,backgroundColor=FFFFFF,noPt=true/simbolos-de-cartas-poker-camiseta-hombre.jpg" class="card-img-top" alt="...">
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Mail: ${element.email}</li>
-    <li class="list-group-item">Phone: ${element.phone}</li>
-    <li class="list-group-item">Username: ${element.username}</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href= ${element.website} class="card-link">Website</a>
-  </div>
-  
-</div>
-  ` 
-})
+  <h3 class="card-title">${palos}${numeros}</h3>
+    
+</div>`
 
-//Mapeo hacia HTML
+  document.querySelector("#java").innerHTML = text
 
-const listPeople= document.querySelector("#list");
-listPeople.innerHTML=textHTML; //Lista
-
-const cardPeople = document.querySelector('#card')
-  cardPeople.innerHTML = cardHTML //Card
- 
 };

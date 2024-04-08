c261f77c0dd6668ff326235a0db82eeedab193e0
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-08 13:09:29 +0000

    modificado

--- src/app.js
+++ src/app.js
@@ -5,22 +5,27 @@ import "./style.css";
 
 window.onload = function () {
   
-  
   let palo = ["♠", "♥", "♣", "♦"];
   
   let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   
+  let paloIndex= palo[Math.floor(Math.random()*palo.length)];
+  let numeroIndex=numero [Math.floor(Math.random()*numero.length)];
+  
+  let color = ( paloIndex === "♥" || paloIndex === "♦") ? "text-danger" : "";
   
   document.querySelector('#cartas').innerHTML = 
   
   
   `
-  <div class="card mb-4 mx-auto mt-5" style="width: 180px;height: 220px;">
-    <div class="card-header text-start"><h1>${palo[Math.floor(Math.random()*palo.length)]}</h1></div>
-    <div class"card-number text-center"><h1>${numero[Math.floor(Math.random()*numero.length)]}</h1></div>
+  <div class="card mb-4 mx-auto mt-2 rounded"  style="width: 280px;height: 300px;">
+    <div class="text-start"><h1 id="header" class="${color}"  >${paloIndex}</h1></div>
+    <div class"text-center"><h1 id="number">${numeroIndex}</h1></div>
+    <div><h1 id="footer" class="${color}">${paloIndex}</h1></div>
   </div>
-  
-  <button type="button" onclick="location.reload();" class="btn btn-warning btn-lg">Generate Random Card</button>
+    
+    <button type="button" onclick="location.reload();" class="btn btn-warning btn-lg">Generate Random Card</button>
+   
   `
           
 };   

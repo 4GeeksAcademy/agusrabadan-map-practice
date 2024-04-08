c53a51313f3dd6281eccd37818e7a4ee67ab79d8
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-08 09:31:31 +0000

    generator

--- src/app.js
+++ src/app.js
@@ -4,33 +4,32 @@ import "./style.css";
 
 
 window.onload = function () {
-
+  
+  
   let palo = ["♠", "♥", "♣", "♦"];
-    
+  
   let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  
+  
+  let color;
+  if(palo === "♥" || palo === "♦"){
+    color= "text-danger";
+  };
+  
+  document.querySelector('#cartas').innerHTML = 
 
-  let text = " "
-  palo.map((element) => {
-    numero.map((item) => {
-      let color;
-        if(element ==="♥" || element ==="♦"){
-          color="text-danger";
-        }
       
-      text +=
       `
-      <div class="card mb-4 mx-3" style="width: 160px;height: 200px;">
-        <div class="card-body">
-          <h5 class="card-title ${color} align-start d-flex">${item}${element}</h5>
-          <img src="https://i.pinimg.com/736x/7b/45/63/7b45638064e62f2e584777e170c53be0.jpg" class="card-img-top" alt="..." style="width: 100px;height: 120px;">
-          <h5 class="card-title ${color} d-flex">${item}${element}</h5>
-        </div>
+      <div class="card mb-4 mx-auto mt-5" style="width: 180px;height: 220px;">
+        
+          <div class="card-header ${color} text-start"><h1>${palo[Math.floor(Math.random()*palo.length)]}</h1></div>
+          <div class"card-number text-center"><h1>${numero[[Math.floor(Math.random()*numero.length)]]}</h1></div>
+          
+        
       </div>
       `
-    })
-  })
+    
+  
       
-  const pokerCards = document.querySelector('#cartas')
-  pokerCards.innerHTML = text
   
   };

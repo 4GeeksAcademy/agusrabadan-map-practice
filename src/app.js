b72e0811ac51613c3b02a76bbbb0e414df48cf3e
Agustin Rabadán Martínez <agusrabadan85@gmail.com>
2024-04-08 10:34:29 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -11,25 +11,16 @@ window.onload = function () {
   let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   
   
-  let color;
-  if(palo === "♥" || palo === "♦"){
-    color= "text-danger";
-  };
-  
   document.querySelector('#cartas').innerHTML = 
-
-      
-      `
-      <div class="card mb-4 mx-auto mt-5" style="width: 180px;height: 220px;">
-        
-          <div class="card-header ${color} text-start"><h1>${palo[Math.floor(Math.random()*palo.length)]}</h1></div>
-          <div class"card-number text-center"><h1>${numero[[Math.floor(Math.random()*numero.length)]]}</h1></div>
+  
+  
+  `
+  <div class="card mb-4 mx-auto mt-5" style="width: 180px;height: 220px;">
+    <div class="card-header text-start"><h1>${palo[Math.floor(Math.random()*palo.length)]}</h1></div>
+    <div class"card-number text-center"><h1>${numero[Math.floor(Math.random()*numero.length)]}</h1></div>
+  </div>
+  
+  <button type="button" onclick="location.reload();" class="btn btn-warning btn-lg">Generate Random Card</button>
+  `
           
-        
-      </div>
-      `
-    
-  
-      
-  
-  };
+};   
